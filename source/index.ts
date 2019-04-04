fa1bb1b7f0ad0f7324c5f087097d2b82213cab8f
MDud <duda915@gmail.com>
2019-04-04 12:53:10 +0200

    added file for testing webpack

--- source/index.ts
+++ source/index.ts
@@ -1,9 +1,14 @@
 import express = require("express");
+import { one } from "./src/boilerplayer";
 
 const app: express.Application = express();
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.get("/one", (req, res) => {
+  res.send(one.toString());
 });
 
 app.listen(3000, () => {
