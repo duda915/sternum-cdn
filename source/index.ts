c5a41572752c186d388dc0be931012fc3f9d67b8
MDud <duda915@gmail.com>
2019-04-04 12:49:14 +0200

    removed no-console rule, added boilerplate code for express

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,11 @@
-export function setup(test: number): number {
-  return test;
-}
+import express = require("express");
 
-console.log(setup(12));
+const app: express.Application = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
